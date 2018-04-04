134c37adb947187a286f5899f56e471fb22ffaee
kiyashko <kiyashkolive@gmail.com>
2018-04-04 22:04:35 +0300

    Предварительная проверка 04.03 22:04

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,6 @@
+
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 250;
 var CLOUD_X = 100;
@@ -8,7 +11,7 @@ var TEXT_GAP = 20;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
@@ -17,7 +20,7 @@ function getRandom() {
   return Math.random();
 }
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -27,18 +30,18 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillStyle = "#000";
+  ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 15);
   ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 35);
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = "#000"
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - TEXT_GAP);
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -46,7 +49,7 @@ window.renderStatistics = function(ctx, names, times) {
       ctx.fillStyle = 'rgba(0,0,255,' + getRandom() + ')';
     }
     ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - TEXT_GAP) - BAR_HEIGHT * times[i] / maxTime, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
-    ctx.fillStyle = "#000"
+    ctx.fillStyle = '#000';
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - TEXT_GAP) - BAR_HEIGHT * times[i] / maxTime - TEXT_GAP);
   }
 };
